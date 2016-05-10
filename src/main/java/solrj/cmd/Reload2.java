b3c10344a881d7e2532209b4786532e4f987ed34
zhifeiyu <lin_gq2014@163.com>
2016-05-10 11:32:35 +0800

    同步代码

--- src/main/java/solrj/cmd/Reload2.java
+++ src/main/java/solrj/cmd/Reload2.java
@@ -14,21 +14,21 @@ import java.util.Map;
  * Created by zfy on 2016-04-29.
  */
 public class Reload2 {
-    private String zkHost = "10.204.76.79:2181,10.204.76.80:2181,10.204.76.42:2181";
-    private String core = "admin";
 
     public void reload(ZSolrClient zClient, String core) throws IOException, SolrServerException {
         CollectionAdminRequest.Reload reload = new CollectionAdminRequest.Reload();
         reload.setCollectionName(core);
 
+        // reload
         CollectionAdminResponse response = reload.process(zClient.getClient());
-        Map<String, NamedList<Integer>> status = response.getCollectionCoresStatus();
+
+        int status = response.getStatus();
 
         System.out.println(status);
     }
 
     public static void main(String[] args){
-        String zkHost = "10.204.76.42:2181";
+        String zkHost = "10.204.76.79:2181,10.204.76.80:2181,10.204.76.42:2181";
         String core = "soft";
         ZSolrClient zClient = new SolrCloudClient(zkHost, core);
         Reload2 cmd = new Reload2();
